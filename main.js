6aaa081c989f787e37cd0c19b220257851b37fb4
sa20122424 <saloxiddin201224@gmail.com>
2024-08-13 18:23:40 +0500

    salom

---
+++ main.js
@@ -0,0 +1,13 @@
+let ism = prompt('ismingizni kiriting')
+
+let id = +prompt('mars id kiriting')
+
+let password = +prompt('Mars password kiriting')
+
+let object = {
+    ism:ism,
+    id:id,
+    password:password,
+}
+
+console.log(object);
